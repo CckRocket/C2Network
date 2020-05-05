a707ef2c11f7e9c3d520fc3a8cf3617b811df633
Your Name <cck_rocket@163.com>
2020-05-05 09:13:18 -0700

    add new node

--- prepare.sh
+++ prepare.sh
@@ -12,3 +12,21 @@ configtxgen -profile InfoChannel -outputAnchorPeersUpdate ./config/InfoChannel/O
 configtxgen -profile InfoChannel -outputAnchorPeersUpdate ./config/InfoChannel/OrgInfo1MSPanchors.tx -channelID infochannel -asOrg OrgInfo1MSP
 
 configtxgen -profile InfoChannel -outputAnchorPeersUpdate ./config/InfoChannel/OrgInfo2MSPanchors.tx -channelID infochannel -asOrg OrgInfo2MSP
+
+#commandchannel config
+configtxgen -profile CommandChannel -outputAnchorPeersUpdate ./config/CommandChannel/OrgCCMSPanchors.tx -channelID commandchannel -asOrg OrgCCMSP
+
+configtxgen -profile CommandChannel -outputAnchorPeersUpdate ./config/CommandChannel/OrgSC1MSPanchors.tx -channelID commandchannel -asOrg OrgSC1MSP
+
+configtxgen -profile CommandChannel -outputAnchorPeersUpdate ./config/CommandChannel/OrgSC2MSPanchors.tx -channelID commandchannel -asOrg OrgSC2MSP
+
+#sharedchannel config
+configtxgen -profile SharedChannel -outputAnchorPeersUpdate ./config/SharedChannel/OrgCCMSPanchors.tx -channelID sharedchannel -asOrg OrgCCMSP
+
+configtxgen -profile SharedChannel -outputAnchorPeersUpdate ./config/SharedChannel/OrgSC1MSPanchors.tx -channelID sharedchannel -asOrg OrgSC1MSP
+
+configtxgen -profile SharedChannel -outputAnchorPeersUpdate ./config/SharedChannel/OrgSC2MSPanchors.tx -channelID sharedchannel -asOrg OrgSC2MSP
+
+configtxgen -profile SharedChannel -outputAnchorPeersUpdate ./config/SharedChannel/OrgInfo1MSPanchors.tx -channelID sharedchannel -asOrg OrgInfo1MSP
+
+configtxgen -profile SharedChannel -outputAnchorPeersUpdate ./config/SharedChannel/OrgInfo2MSPanchors.tx -channelID sharedchannel -asOrg OrgInfo2MSP
